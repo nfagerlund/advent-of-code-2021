46ed369eaf5bbe60d93062d8e8a995b940f2a9bb
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-12-08 10:07:53 -0800

    a cleanup

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,7 +1,12 @@
 use std::fs;
 
+fn load_inputs(dataset: &str) -> std::io::Result<String> {
+    let file = format!("./inputs/{}.txt", dataset);
+    fs::read_to_string(file)
+}
+
 fn main() {
-    let inputs = fs::read_to_string("./inputs/day1a.txt").unwrap();
+    let inputs = load_inputs("day1a").unwrap();
     let first = inputs.lines().next().unwrap();
     println!("{}", first);
 }
