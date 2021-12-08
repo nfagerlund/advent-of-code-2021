9474f162a9395b100d7dd584c225fccfa1ff03ad
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-12-08 13:43:26 -0800

    yey this worked.

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -5,8 +5,23 @@ fn load_inputs(dataset: &str) -> std::io::Result<String> {
     fs::read_to_string(file)
 }
 
+// Count how many times the depth *increases*. Doesn't matter how much it
+// increases by, doesn't matter how many measurements there were.
+// Input is numbers separated by newlines.
 fn main() {
     let inputs = load_inputs("day1a").unwrap();
-    let first = inputs.lines().next().unwrap();
-    println!("{}", first);
+    let mut last_sounding: Option<i32> = None;
+    let mut increases = 0;
+    for sounding in inputs.lines()
+        .map(|l| { i32::from_str_radix(l, 10).unwrap() })
+    {
+        if let Some(previous) = last_sounding {
+            if sounding > previous {
+                increases += 1;
+            }
+        }
+        last_sounding = Some(sounding);
+    }
+
+    println!("Depth increased {} times", increases);
 }
