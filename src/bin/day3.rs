3c6e13c08aa12c2a39bd83aaa85ab20c74897a67
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-12-08 23:39:33 -0800

    hell yes, the test worked and the code worked

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -6,8 +6,64 @@ fn main() {
 }
 
 fn part_one(inputs: String) -> i32 {
-    println!("hi.");
-    let multiplied = 0;
+    let mut multiplied = 0;
+    // Right. So. This one is goofy.
+    // It looks like we can get away with only tracking a running sum for each
+    // binary digit. And the epsilon rate is just the gamma rate with every bit
+    // flipped.
+    // Uhhhhh we have 1000 numbers to work with, and that is an even number,
+    // sooooooo... there's a chance of a tie, and the problem statement doesn't
+    // say how to break a tie.
+    // I say we panic on tie.
+    // SO: a running sum integer for each digit. We ++ for 1, and -- for 0.
+    // At end, if sum > 0 gamma gets 1, if sum < 0 gamma gets 0, if sum == 0 panic.
+    // Then we need to flip bits for epsilon.
+    // Then we need to parse int from str of radix 2.
+    // Then multiply.
+
+    // K let's do this. char_indices is the inner iterator we want, and we can
+    // collect our stuff in a vec.
+
+    // I'm going to initialize the whole thing with zeroes first, just to make
+    // the subsequent logic cleaner. Assume each number in input is same length.
+    let input_width = inputs.lines().next().unwrap().len();
+    let mut digit_accumulators: Vec<i32> = vec![0; input_width];
+
+    for line in inputs.lines() {
+        for (index, digit) in line.char_indices() {
+            match digit {
+                '0' => digit_accumulators[index] -= 1,
+                '1' => digit_accumulators[index] += 1,
+                _ => panic!("unexpected digit in input: {}", digit),
+            }
+        }
+    }
+    // k. At this point, we should have what we need in terms of counts...
+    println!("{:?}", &digit_accumulators);
+    // OK! At least there's no ties!
+    // Now... Maybe let's use bit shifting, if I can do so without wrecking self.
+    let mut gamma = 0;
+    let mut epsilon = 0;
+    for place in digit_accumulators {
+        gamma <<= 1;
+        epsilon <<= 1;
+        match place {
+            0 => panic!("a digit frequency count tied, and I don't know how to break it"),
+            (i32::MIN..=-1) => {
+                // negative: gamma gets 0, epsilon gets 1
+                epsilon += 1;
+            },
+            (1..=i32::MAX) => {
+                // positive: gamma gets 1, epsilon gets 0
+                gamma += 1;
+            },
+        }
+    }
+    // ok??
+    multiplied = gamma * epsilon;
+    println!("Gamma: {0:b} ({0})", gamma);
+    println!("Epsilon: {0:b} ({0})", epsilon);
+    println!("Multiplied: {}", multiplied);
 
     multiplied
 }
