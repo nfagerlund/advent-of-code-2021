ddfd9da903f592811a45c4e627b11ae30284ef1e
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-12-08 21:47:50 -0800

    Yey that works. (day 2 pt 1)

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,5 +1,27 @@
 use advent21::*;
 
 fn main() {
-  println!("sup");
+    part_one()
+}
+
+fn part_one() {
+    let inputs = load_inputs("day2").unwrap();
+    // OK, we have lines, we need to split them on spaces, then we need to switch
+    // on commands, and we need to keep two running totals. Easy peasy.
+    let mut x_pos = 0;
+    let mut depth = 0;
+    for line in inputs.lines() {
+        // Once again, I'm very willing to panic in the disco.
+        let (command, units) = line.split_once(' ').unwrap();
+        let units = i32::from_str_radix(units, 10).unwrap();
+        match command {
+            "up" => depth -= units,
+            "down" => depth += units,
+            "forward" => x_pos += units,
+            _ => panic!("unrecognized command: {}", command),
+        }
+    }
+    println!("Horizontal position: {}", x_pos);
+    println!("Depth: {}", depth);
+    println!("Multiplied: {}", x_pos * depth);
 }
