a058f1c50f53a924f951cbc2723b2c4bb0d27abf
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-12-11 13:49:23 -0800

    OK, I think we got a usable input format

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,4 +1,5 @@
 use advent21::*;
+use std::cmp;
 
 // Crab submarines day! inputs are horizontal positions. Find the slot that will
 // take the least total units of fuel to get everyone to.
@@ -12,8 +13,31 @@ fn main() {
 fn part_two(_inputs: &str) {}
 
 fn part_one(inputs: &str) -> usize {
-
+    let _ = parse_inputs(inputs);
     0
+}
+
+// ...I think we need a vec for this, bc who knows what all horizontal positions
+// they might have. (Ah yeah, it's out in the thousands somewhere.) Or,
+// alternately, I could just find the max first. Especially since I think I want
+// to initialize it zeroed-out anyway. Oh, but we won't know the length before
+// running the function, so we won't know what return type to use. Okay, vec it
+// is!
+// - The x-axis is unsigned it looks like.
+// - I think we want each slot to represent the number of crabs at that x
+//   position (so, usize).
+fn parse_inputs(inputs: &str) -> Vec<usize> {
+    let inputs = inputs.trim();
+    let converted_inputs: Vec<usize> = inputs.split(',').map(usize_or_die).collect();
+    let max_x_pos = converted_inputs.iter()
+        .fold(0usize, |max, val| cmp::max(max, *val) );
+    let mut outputs = vec![0usize; max_x_pos + 1];
+        // ah ah, zero-indexing, gotta make sure outputs[max] is a valid slot.
+    for crab in converted_inputs {
+        outputs[crab] += 1;
+    }
+    println!("Parsed inputs to vec of crab counts at each x position:\n{:?}", &outputs);
+    outputs
 }
 
 #[cfg(test)]
