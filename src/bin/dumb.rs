0ed987953ef2111ca166b6ef2377a3c3d57197b1
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-12-11 12:22:42 -0800

    silence noisy warnings in dumb

--- src/bin/dumb.rs
+++ src/bin/dumb.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("hi");
-    let mut inputs = [18, 9];
+    let inputs = [18, 9];
     // inputs.sort();
     // println!("sorted inputs: {:#?}", &inputs);
     // let thing: Vec<i32> = (inputs[0]..=inputs[1]).collect();
@@ -8,7 +8,7 @@ fn main() {
         // What I learned: I think I was just experimenting and trying to find
         // something that'd work for day 5.1.
 
-    // println!("beyond the veil: {}", inputs[3]);
+    println!("beyond the veil: {}", inputs[3]);
         // What I learned: out-of-bounds array access panics on runtime, BUT, if
         // the compiler KNOWS that a particular access will ALWAYS panic (like
         // this one, which uses a constant expression), it'll just call it a
