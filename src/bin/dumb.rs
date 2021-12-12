fffcf6479685ebcae7a1664cfc6d89b5cf5b3ae7
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-12-12 12:13:49 -0800

    testing HashSet in dumb

--- src/bin/dumb.rs
+++ src/bin/dumb.rs
@@ -1,6 +1,8 @@
+use std::collections::HashSet;
+
 fn main() {
     println!("hi");
-    let inputs = [18, 9];
+    // let inputs = [18, 9];
     // inputs.sort();
     // println!("sorted inputs: {:#?}", &inputs);
     // let thing: Vec<i32> = (inputs[0]..=inputs[1]).collect();
@@ -8,10 +10,23 @@ fn main() {
         // What I learned: I think I was just experimenting and trying to find
         // something that'd work for day 5.1.
 
-    println!("beyond the veil: {}", inputs[3]);
+    // println!("beyond the veil: {}", inputs[3]);
         // What I learned: out-of-bounds array access panics on runtime, BUT, if
         // the compiler KNOWS that a particular access will ALWAYS panic (like
         // this one, which uses a constant expression), it'll just call it a
         // compiler error instead and stop you. (configurable with
         // `#[deny(unconditional_panic)]`)
+
+    // testing HashSet
+    let mut first = HashSet::new();
+    let mut second = HashSet::new();
+    let mut third = HashSet::new();
+    first.insert('a');
+    first.insert('c');
+    second.insert('c');
+    second.insert('a');
+    third.insert('b');
+    println!("first == second: {}", first == second);
+    println!("first == third: {}", first == third);
+        // what I learned: HashSet implements equality like I'd expect.
 }
