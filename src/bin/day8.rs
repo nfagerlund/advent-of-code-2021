24676817fad7fb07f1c82b355af7182234f388fe
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-12-11 18:06:15 -0800

    Mmmm okay, wip

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use advent21::*;
 
 // the case of the scrambled seven-segment displays
@@ -21,7 +22,44 @@ fn main() {
     part_two(&inputs);
 }
 
-fn part_two(_inputs: &str) {}
+// All RIGHT! Now I need to ACTUALLY solve a puzzle!
+// Translate every four-digit number, and sum them up.
+// Right, first off, or I guess last off, the sequences are randomly ordered
+// such that fcadb == fbcad. So, gotta sort/normalize them in between the
+// decoding and translating stages.
+
+// OK. There's three 6s, and three 5s.
+// sixes: 0, 6, 9
+// fives: 2, 5, 3
+// I guess I'm looking for segments in common! Let's consider globally first.
+// The three horizontal lines -- present in 2, 3, 5, 6, 9. Middle one is absent in 0.
+    // 7 has the top line, plus the vertical lines from 1.
+// The four vertical lines: all present in 0. 6 and 9 each have three. 2, 5, 3 each have two.
+    // 2 and 5 have non-overlapping sets.
+    // 3 has the same ones that 1 has!
+    // 9 has the same ones that 4 has!
+// Ok, well this is interesting... maybe a frequency count is a good first move, tho. Let's use the segment names from the example:
+//  aaaa
+// b    c
+// b    c
+//  dddd
+// e    f
+// e    f
+//  gggg
+// Number of digits each segment appears in:
+// horizontal lines:
+// a: 8, d: 7, g: 7
+// vertical lines:
+// b: 6, e: 4, c: 8, f: 9
+// Oh wow! So, you can figure out f (vbr), e (vbl), and b (vul) from freqs alone! That's 3 out of 4 verticals.
+// For the final vertical, c (vur, x8): it and f are the only segments present in 1. So that's all them.
+// There's only one other x8, so that's a (ht).
+// d (hm) is the only horizontal segment that's absent from a six-segment digit. and g (hb) is whatever's left.
+fn part_two(inputs: &str) -> usize {
+    let the_stuff = parse_inputs_weirdly(inputs);
+    println!("the stuff is here\n{:#?}", &the_stuff);
+    0
+}
 
 // How many times do the digits 1, 4, 7, or 8 appear?
 fn part_one(inputs: &str) -> usize {
@@ -44,6 +82,57 @@ fn part_one(inputs: &str) -> usize {
 // fuck it. (ten_signals, four_digits).
 fn parse_inputs_naively(inputs: &str) -> Vec<(&str, &str)> {
     inputs.lines().map(|line| line.split_once(" | ").unwrap()).collect()
+}
+
+enum Segment {
+    Ht,
+    Hm,
+    Hb,
+    Vul,
+    Vur,
+    Vbl,
+    Vbr,
+    Unknown,
+}
+
+struct Display {
+    digit_signals: HashMap<String, usize>,
+    readout: [String; 4],
+}
+
+impl Display {
+    fn to_number(&self) -> usize {
+        self.digit_signals[&self.readout[0]] * 1000
+        +
+        self.digit_signals[&self.readout[1]] * 100
+        +
+        self.digit_signals[&self.readout[2]] * 10
+        +
+        self.digit_signals[&self.readout[3]]
+    }
+}
+
+fn parse_inputs_weirdly(inputs: &str) -> Vec<(Vec<String>, Vec<String>)> {
+    let tuples = parse_inputs_naively(inputs);
+    let vecs_of_sorted_strings: Vec<(Vec<String>, Vec<String>)> = tuples.iter()
+        .map(|(ten_signals, four_digits)| {
+            let signals: Vec<String> = ten_signals.split(' ')
+                .map(sort_to_string).collect();
+            let digits: Vec<String> = four_digits.split(' ')
+                .map(sort_to_string).collect();
+            (signals, digits)
+        }).collect();
+    vecs_of_sorted_strings
+}
+
+fn sort_to_string(s: &str) -> String {
+    let mut chars: Vec<char> = s.chars().collect();
+    chars.sort();
+    let mut result = String::new();
+    for chr in chars {
+        result.push(chr);
+    }
+    result
 }
 
 #[cfg(test)]
@@ -70,7 +159,7 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 
     #[test]
     fn example_part_two() {
-        let answer = ();
+        let answer = 61229;
         let result = part_two(EXAMPLE);
         assert_eq!(result, answer);
     }
