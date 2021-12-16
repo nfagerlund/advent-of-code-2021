31cb9a4a97dadb5c8252fa73b5238b54e2af6e5b
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-12-16 11:15:46 -0800

    Refactor Grid out into lib (`advent21::grid::{Grid,Tile}`)
    Had to turn that `Grid<Octopus>` impl into a trait, but otherwise that worked fine

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,183 @@
 use std::fs;
 
 pub fn load_inputs(dataset: &str) -> std::io::Result<String> {
-  let file = format!("./inputs/{}.txt", dataset);
-  fs::read_to_string(file)
+    let file = format!("./inputs/{}.txt", dataset);
+    fs::read_to_string(file)
 }
 
 // Get a number or die trying
 pub fn usize_or_die(s: &str) -> usize {
-  usize::from_str_radix(s, 10).unwrap()
+    usize::from_str_radix(s, 10).unwrap()
 }
 
 pub fn i32_or_die(s: &str) -> i32 {
-  i32::from_str_radix(s, 10).unwrap()
+    i32::from_str_radix(s, 10).unwrap()
+}
+
+pub mod grid {
+    /// A grid using game-like coordinates (i.e. 0,0 => upper left corner).
+    #[derive(Debug)]
+    pub struct Grid<T> {
+        data: Vec<Vec<T>>, // vec of rows of values
+    }
+
+    pub type Tile = (usize, usize);
+    enum Axis {
+        X,
+        Y,
+    }
+
+    impl<T> Grid<T> {
+        pub fn new(data: Vec<Vec<T>>) -> Grid<T> {
+            Grid { data }
+        }
+
+        pub fn height(&self) -> usize { self.data.len() }
+        // Constructor is in charge of not giving us inconsistent widths! I'm still not checking.
+        pub fn width(&self) -> usize { self.data[0].len() }
+
+        pub fn tiles(&self) -> Box<dyn Iterator<Item = Tile>> {
+            let height = self.height();
+            let width = self.width();
+            let tiles_iter = (0..height).map(move |y| {
+                (0..width).map(move |x| (x, y))
+            }).flatten();
+            Box::new(tiles_iter)
+        }
+
+        /// Returns an immutable reference to the value in a tile. `None` means the
+        /// coordinates in the requested tile are out of bounds; in curent
+        /// implementation, every tile has to have a valid value, so you gotta use
+        /// an Option of your own if some tiles need empty values.
+        pub fn get_tile_value(&self, tile: Tile) -> Option<&T> {
+            let (x, y) = tile;
+            match self.data.get(y) { // row
+                None => None,
+                Some(row) => {
+                    match row.get(x) {
+                        None => None,
+                        Some(value) => Some(value),
+                    }
+                },
+            }
+        }
+
+        /// Same as before but mutable reference.
+        pub fn get_tile_value_mut(&mut self, tile: Tile) -> Option<&mut T> {
+            let (x, y) = tile;
+            match self.data.get_mut(y) { // row
+                None => None,
+                Some(row) => {
+                    match row.get_mut(x) {
+                        None => None,
+                        Some(value) => Some(value),
+                    }
+                },
+            }
+        }
+
+        // Smash this neighbors thing apart a bit:
+
+        /// Like usize.checked_sub(), except based on the grid size.
+        fn more(&self, coord: usize, axis: Axis) -> Option<usize> {
+            let axis_len = match axis {
+                Axis::X => self.width(),
+                Axis::Y => self.height(),
+            };
+            if coord + 1 < axis_len {
+                Some(coord + 1)
+            } else {
+                None
+            }
+        }
+
+        fn get_neighbor_n(&self, tile: Tile) -> Option<Tile> {
+            let (x, y) = tile;
+            if let Some(y) = y.checked_sub(1) {
+                return Some((x, y));
+            }
+            None
+        }
+        fn get_neighbor_w(&self, tile: Tile) -> Option<Tile> {
+            let (x, y) = tile;
+            if let Some(x) = x.checked_sub(1) {
+                return Some((x, y));
+            }
+            None
+        }
+        fn get_neighbor_nw(&self, tile: Tile) -> Option<Tile> {
+            let (x, y) = tile;
+            if let Some(y) = y.checked_sub(1) {
+                if let Some(x) = x.checked_sub(1) {
+                    return Some((x, y));
+                }
+            }
+            None
+        }
+        fn get_neighbor_s(&self, tile: Tile) -> Option<Tile> {
+            let (x, y) = tile;
+            if let Some(y) = self.more(y, Axis::Y) {
+                return Some((x, y));
+            }
+            None
+        }
+        fn get_neighbor_e(&self, tile: Tile) -> Option<Tile> {
+            let (x, y) = tile;
+            if let Some(x) = self.more(x, Axis::X) {
+                return Some((x, y));
+            }
+            None
+        }
+        fn get_neighbor_se(&self, tile: Tile) -> Option<Tile> {
+            let (x, y) = tile;
+            if let Some(y) = self.more(y, Axis::Y) {
+                if let Some(x) = self.more(x, Axis::X) {
+                    return Some((x, y));
+                }
+            }
+            None
+        }
+        fn get_neighbor_ne(&self, tile: Tile) -> Option<Tile> {
+            let (x, y) = tile;
+            if let Some(y) = y.checked_sub(1) {
+                if let Some(x) = self.more(x, Axis::X) {
+                    return Some((x, y));
+                }
+            }
+            None
+        }
+        fn get_neighbor_sw(&self, tile: Tile) -> Option<Tile> {
+            let (x, y) = tile;
+            if let Some(y) = self.more(y, Axis::Y) {
+                if let Some(x) = x.checked_sub(1) {
+                    return Some((x, y));
+                }
+            }
+            None
+        }
+
+        // get coords
+        pub fn get_neighbors_cardinal(&self, tile: Tile) -> Vec<Option<Tile>> {
+            vec![
+                self.get_neighbor_n(tile),
+                self.get_neighbor_e(tile),
+                self.get_neighbor_s(tile),
+                self.get_neighbor_w(tile),
+            ]
+        }
+        pub fn get_neighbors_ordinal(&self, tile: Tile) -> Vec<Option<Tile>> {
+            vec![
+                self.get_neighbor_ne(tile),
+                self.get_neighbor_nw(tile),
+                self.get_neighbor_se(tile),
+                self.get_neighbor_sw(tile),
+            ]
+        }
+        pub fn get_neighbors_all(&self, tile: Tile) -> Vec<Option<Tile>> {
+            let mut neighbors = self.get_neighbors_cardinal(tile);
+            let mut ordinal_neighbors = self.get_neighbors_ordinal(tile);
+            neighbors.append(&mut ordinal_neighbors);
+            neighbors
+        }
+    }
 }
