151e7dd031d728fe0a545fcac4fd4a8634aa856c
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-12-17 10:49:11 -0800

    Here's parsing

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,5 +1,7 @@
 use advent21::*;
+use std::collections::HashMap;
 
+// the one with inserting atoms into a molecule
 fn main() {
     let inputs = load_inputs("day14").unwrap();
     part_one(&inputs);
@@ -8,9 +10,45 @@ fn main() {
 
 fn part_two(_inputs: &str) {}
 
+// simultaneously insert the indicated element between each pair of elements. Do
+// this 10 times, then subtract the least common element from the most common
+// element.
+// I would like to cheat in the following ways:
+// - Re-use just two vecs and take advantage of their auto-resizing.
+// - Do something funky with the "pairs" issue.
 fn part_one(inputs: &str) -> usize {
+    let (template, rules) = parse_inputs(inputs);
+    println!("the stuff is here:");
+    dbg!(&template);
+    dbg!(&rules);
 
     0
+}
+
+fn parse_inputs(inputs: &str) -> (Vec<char>, RulesDict) {
+    let (template, rules) = inputs.split_once("\n\n").unwrap();
+    (parse_polymer_template(template), parse_insertion_rules(rules))
+}
+
+type RulesDict = HashMap<char, HashMap<char, char>>;
+
+// rules[first][second] = insertion
+fn parse_insertion_rules(inputs: &str) -> RulesDict {
+    let mut rules: RulesDict  = HashMap::new();
+
+    for line in inputs.lines() {
+        let (pair, insertion) = line.split_once(" -> ").unwrap();
+        let first = pair.chars().next().unwrap();
+        let second = pair.chars().last().unwrap();
+        let insertion = insertion.chars().next().unwrap();
+        rules.entry(first).or_insert(HashMap::new()).insert(second, insertion);
+    }
+
+    rules
+}
+
+fn parse_polymer_template(inputs: &str) -> Vec<char> {
+    inputs.chars().collect()
 }
 
 #[cfg(test)]
