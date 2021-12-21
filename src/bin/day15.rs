6fdcdc5582df5c6a5c594fb80f82b0f9dd45aeb2
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-12-21 11:03:03 -0800

    Start in on a Path type

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,5 +1,8 @@
 use advent21::*;
 use advent21::grid::*;
+use std::collections::BinaryHeap;
+use std::fmt::Binary;
+use std::cmp::Ordering;
 
 // The one where yup, it's another grid, and you gotta do pathfinding to get the
 // lowest total score from top-left to bottom-right. It's totally A*.
@@ -16,6 +19,30 @@ fn part_one(inputs: &str) -> usize {
     // println!("The stuff is here: \n{:#?}", &grid);
     0
 }
+
+#[derive(Eq, PartialEq)]
+struct Path {
+    route: Vec<Tile>,
+    cost: usize,
+}
+
+// Per the BinaryHeap docs, it's only a max heap but I can get a min heap by
+// defining a custom Ord. And I need that anyway because I only want to compare
+// paths by cost!
+impl Ord for Path {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // reversed
+        other.cost.cmp(&self.cost)
+    }
+}
+impl PartialOrd for Path {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        // but don't double-reverse.
+        Some(self.cmp(&other))
+    }
+}
+// and then I THINK I can get away with just derived eqs, because I do want to
+// compare all fields on those.????
 
 fn parse_inputs (inputs: &str) -> Grid<usize> {
     let grid_data: Vec<Vec<usize>> = inputs.lines().map(
