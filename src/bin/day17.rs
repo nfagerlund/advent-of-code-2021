d09c3c77f7817a876a12d24afdb2621481fe0fcd
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-12-26 21:09:43 -0800

    OK, not sure how to do the targeting yet, but at least there's this

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -16,6 +16,23 @@ fn part_one(inputs: &str) -> i32 {
     0
 }
 
+// OK, I derived the equations, at least. Using "w" as "time step":
+// y = w * v_y - sum(1..(w - 1))
+// and then x has a bail-out condition but is otherwise same idea.
+fn plot_y(v_y: i32, time: i32) -> i32 {
+    // it's just that doing division with ints requires some concentration, that's all.
+    time * v_y - (time * (time - 1))/2
+}
+fn plot_x(v_x: i32, time: i32) -> i32 {
+    if time < v_x {
+        plot_y(v_x, time)
+    } else {
+        // Once the velocity is spent on friction, X position stays the same
+        // instead of reversing.
+        plot_y(v_x, v_x)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -33,5 +50,11 @@ mod tests {
         let answer = ();
         let result = part_two(EXAMPLE);
         assert_eq!(result, answer);
+    }
+
+    #[test]
+    fn rounding_plots() {
+        assert_eq!(plot_y(9, 5), 35);
+        assert_eq!(plot_y(9, 6), 39);
     }
 }
